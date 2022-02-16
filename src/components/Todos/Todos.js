c95abcc56f1efef76e535b5b74c0bddd1b9c7bbc
Sachin Tripathi <sachin@incubyte.co>
2022-02-16 17:04:11 +0530

    ENH: Added http calls to fetch open and closed todos

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,18 +1,39 @@
+import axios from "axios";
+import React from "react";
+
 import ListTodo from "./ListTodo";
 import './Todos.css'
 
+const baseURL = 'http://localhost:8080/todos'
+
 const Todos = () => {
-    // TODO: Fetch todos from micronaut backend
-    const todos = [
-        {id: 1, title: "Todo 1", status: "OPEN"},
-        {id: 2, title: "Todo 2", status: "CLOSED"}
-    ]
+    const [openTodos, setOpenTodos] = React.useState([]);
+    const [closedTodos, setClosedTodos] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get(`${baseURL}/open`).then((response) => {
+            setOpenTodos(response.data);
+        });
+    }, []);
+
+    React.useEffect(() => {
+        axios.get(`${baseURL}/closed`).then((response) => {
+            setClosedTodos(response.data);
+        });
+    }, []);
 
     return (
         <div className="todo-container">
             <div className="todos">
                 {/*TODO: Create a new component to add new todos*/}
-                <ListTodo todos={todos}/>
+            </div>
+            <div className="todos">
+                <h1>Open Todos</h1>
+                <ListTodo todos={openTodos}/>
+            </div>
+            <div className="todos">
+                <h1>Closed Todos</h1>
+                <ListTodo todos={closedTodos}/>
             </div>
         </div>
     )
