8dd2f56d5bb218b5e9c389b7dc27941de3dcf35d
Jaymin Shah <shahjaymin2101@gmail.com>
2022-02-16 17:00:35 +0530

    ENH: Adding http call to fetch closed todos

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -6,17 +6,33 @@ import axios from "axios";
 const Todos = () => {
     // TODO: Fetch todos from micronaut backend
     const [openTodo, setOpenTodo] = React.useState([]);
+    const [closedTodo, setClosedTodo] = React.useState([]);
+
     useEffect(() => {
         // GET request using axios inside useEffect React hook
         axios.get('http://localhost:8080/todos/open')
             .then(response => setOpenTodo(response.data));
     }, []);
 
+    useEffect(() => {
+        // GET request using axios inside useEffect React hook
+        axios.get('http://localhost:8080/todos/closed')
+            .then(response => setClosedTodo(response.data));
+    }, []);
+
     return (
+
         <div className="todo-container">
+            <h1>OPEN TODOS</h1>
             <div className="todos">
                 {/*TODO: Create a new component to add new todos*/}
                 <ListTodo todos={openTodo}/>
+            </div>
+            <br/>
+            <h1>CLOSED TODOS</h1>
+            <div className="todos">
+                {/*TODO: Create a new component to add new todos*/}
+                <ListTodo todos={closedTodo}/>
             </div>
         </div>
     )
