285504ce779106d5de17c93367681f4c3b25fec1
Utsav Varia <utsav99varia@gmail.com>
2022-02-16 16:42:22 +0530

    ENH: Adding http call to fetch todos

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -2,7 +2,6 @@ import ListTodo from "./ListTodo";
 import './Todos.css'
 
 const Todos = () => {
-    // TODO: Fetch todos from micronaut backend
     const todos = [
         {id: 1, title: "Todo 1", status: "OPEN"},
         {id: 2, title: "Todo 2", status: "CLOSED"}
