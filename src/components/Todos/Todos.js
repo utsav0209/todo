452f06f2bc0c70578a2d8dfc96dd95e5b7310f22
Nirav Chavda <nmchavda99@gmail.com>
2022-02-16 16:51:55 +0530

    ENH: Fetch Todo's from API through HTTP requests

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,18 +1,33 @@
 import ListTodo from "./ListTodo";
 import './Todos.css'
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 const Todos = () => {
     // TODO: Fetch todos from micronaut backend
-    const todos = [
-        {id: 1, title: "Todo 1", status: "OPEN"},
-        {id: 2, title: "Todo 2", status: "CLOSED"}
-    ]
+
+    let [openTodos, setOpenTodos] = useState([]);
+    let [closedTodos, setClosedTodos] = useState([]);
+
+    useEffect(() => {
+        const fetchTodos = async () => {
+            const {data: allOpenTodos} = await axios.get("http://localhost:8080/todos/open");
+            setOpenTodos(allOpenTodos);
+
+            const {data: allClosedTodos} = await axios.get("http://localhost:8080/todos/closed");
+            setClosedTodos(allClosedTodos);
+        }
+        fetchTodos();
+    }, []);
+
 
     return (
         <div className="todo-container">
+            {openTodos.length < 1 && <div>Loading...</div>}
+
             <div className="todos">
                 {/*TODO: Create a new component to add new todos*/}
-                <ListTodo todos={todos}/>
+                <ListTodo todos={[...openTodos, ...closedTodos]}/>
             </div>
         </div>
     )
